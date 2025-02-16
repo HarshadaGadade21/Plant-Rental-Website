f55d68965b953d4c0b3942efac9b628a1b18a63b
RutujaGarje <garjerutuja04@gmail.com>
2025-02-16 13:12:24 +0530

    order details added

--- app/my-orders.php
+++ app/my-orders.php
@@ -22,7 +22,7 @@ $query = mysqli_query($con, "SELECT * FROM Orders WHERE customer_name='$username
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Orders</title>
-    <link rel="stylesheet" href="..\resources\css\styles.css">
+    <link rel="stylesheet" href="..\resources\css\my-orders.css">
 </head>
 
 <body>
